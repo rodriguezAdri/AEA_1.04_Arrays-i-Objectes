77e644cb10364ab2569083ce9edfb7e79a4e7911
adrianRodriguez77 <adrian.rodriguez.vegas.7e6@itb.cat>
2024-10-03 17:41:18 +0200

    Practica acabado

--- activitat3.js
+++ activitat3.js
@@ -35,3 +35,38 @@ function mayorDe18(preu){
 const edats = [16, 21, 18, 24, 12, 30].filter(mayorDe18);
 
 document.getElementById("filter").textContent = `Les edats majors de 18 són; ${edats.join(', ')}.`;
+
+//5
+const notes = [7, 9, 5, 10, 8];
+
+function obtenerSuma(total, num) {
+    return total + Math.round(num);
+  }
+
+const suma = notes.reduce(obtenerSuma, 0);
+const mitjana = suma / notes.length; 
+document.getElementById("reduce").textContent = `La mitjana de les notes és: ${mitjana}`;
+
+//6
+function objecteProducte(nom, preu, quantitat) {
+    const preuDescompte = preu*0.95;
+    return {
+        nom: nom,
+        preu: preuDescompte.toFixed(2),
+        quantitat: quantitat
+    }
+}
+const producte = objecteProducte("Campus", 85.99, 1);
+document.getElementById("creaObjecte").textContent = `Nom: ${producte.nom}, Preu: ${producte.preu} €, Quantitat: ${producte.quantitat}`;
+
+//7
+const cotxe = {
+    marca: 'Bmw',
+    model: 'M3',
+    preu: 53.154
+};
+cotxe.any = 2023;
+
+document.getElementById("modificaObjecte").textContent = `${cotxe.marca}, ${cotxe.model}, ${cotxe.preu} €, ${cotxe.any} `
+
+
